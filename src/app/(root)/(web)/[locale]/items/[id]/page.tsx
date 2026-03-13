b648511596d9d7dc82cbef008dacfab15f0e5119
Andrew <andreytr449@gmail.com>
2026-03-13 20:32:16 +0200

    feat: add dynamic metadata to movie page

--- src/app/(root)/(web)/[locale]/items/[id]/page.tsx
+++ src/app/(root)/(web)/[locale]/items/[id]/page.tsx
@@ -1,12 +1,28 @@
 import { getMovieById } from "@/app/entities/api/movies/movies.api";
 import { MovieModule } from "@/app/modules/movie";
 import NotFoundMoviePage from "../not-found";
+import { getTranslations } from "next-intl/server";
 
-export default async function ItemPage({
-  params,
-}: {
+type Props = {
   params: Promise<{ id: string }>;
-}) {
+};
+
+export async function generateMetadata({ params }: Props) {
+  const { id } = await params;
+  const { results, success } = await getMovieById(id);
+  if (!success || !results) {
+    const t = await getTranslations("NotFoundMovie");
+    return {
+      title: t("label"),
+    };
+  }
+
+  return {
+    title: `${results.title}`,
+  };
+}
+
+export default async function ItemPage({ params }: Props) {
   const { id } = await params;
   const { results, success } = await getMovieById(id);
 
