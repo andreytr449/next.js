4080821e1dd977200bd64a942363080af9125691
Andrew <andreytr449@gmail.com>
2026-03-20 19:49:47 +0200

    feat: add responsive header and refactor project structure

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,27 +1,13 @@
-import type { Metadata } from 'next'
 import { ReactNode } from 'react'
-import { Geist, Geist_Mono } from 'next/font/google'
 import '@/config/styles/globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Next.js app',
-  description: 'Next.js application',
+  icons: {
+    icon: '/icon.ico',
+  },
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} dark font-sans antialiased`}>{children}</body>
-    </html>
-  )
+  return children
 }
--- src/app/widgets/header/header.component.tsx
+++ src/app/widgets/header/header.component.tsx
@@ -10,14 +10,14 @@ import { AuthButton } from '@/app/features/auth-button'
 export const Header = async () => {
   const t = await getTranslations('Header')
   return (
-    <header className='bg-background/80 sticky top-0 z-50 mx-28 rounded-4xl border border-b-gray-900 backdrop-blur-md'>
+    <header className='bg-background/80 sticky top-0 z-50 mx-3 rounded-4xl border border-b-gray-900 backdrop-blur-md md:mx-28'>
       <div className='mx-auto flex max-w-7xl items-center justify-between gap-8 px-4 py-7 sm:px-6'>
         <div className='text-muted-foreground flex flex-1 items-center gap-8 font-medium md:justify-center lg:gap-16'>
           <Link href='/'>
             <Logo className='text-foreground gap-3' />
           </Link>
           {navigationData.map((item) => (
-            <Link key={item.key} href={item.href} className='hover:text-primary max-md:hidden'>
+            <Link key={item.key} href={item.href} className='hover:text-primary max-sm:hidden'>
               {t(item.key)}
             </Link>
           ))}
